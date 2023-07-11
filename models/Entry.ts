5203834f5eda07d617d1a4125299e0ad97eadc3c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-11 11:12:17 -0600

    Add entry model

---
+++ models/Entry.ts
@@ -0,0 +1,27 @@
+import mongoose, { Model, Schema } from 'mongoose';
+
+import { Entry } from '../interfaces';
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['pending', 'in-progress', 'completed'],
+      message: '{VALUE} no es un estado válido',
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model<IEntry>('Entry', entrySchema);
+
+export default EntryModel;
