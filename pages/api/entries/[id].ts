b1ec0e074f6a5052f622514e2c0a33392a5f3c14
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-11 16:03:22 -0600

    Add api functionality to update an entry

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import mongoose from 'mongoose';
+
+import { db } from '../../../database';
+import { Entry, IEntry } from '../../../models';
+
+type Data = { message: string } | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: `Invalid id: ${id}` });
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res);
+
+    default:
+      return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
+
+async function updateEntry(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { id } = req.query;
+
+  await db.connect();
+
+  const entryToUpdate = await Entry.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnect();
+    return res.status(404).json({ message: `Entry not found: ${id}` });
+  }
+
+  const {
+    description = entryToUpdate.description,
+    status = entryToUpdate.status,
+  } = req.body;
+
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      { description, status },
+      { runValidators: true, new: true },
+    );
+    await db.disconnect();
+
+    return res.status(200).json(updatedEntry!);
+  } catch (error: any) {
+    await db.disconnect();
+    console.log(error);
+
+    if (error.errors.status.properties.message)
+      return res.status(400).json({ message: 'Invalid status' });
+
+    return res.status(500).json({ message: 'Error creating entry' });
+  }
+}
